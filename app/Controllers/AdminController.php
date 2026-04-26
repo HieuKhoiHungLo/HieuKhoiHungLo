4854ffa18cf9064e24473c6490f3110819c3c84a
HieuKhoiHungLo <kiennt@hvu.edu.vn>
2026-04-27 01:31:56 +0700

    Add debug info to stats API

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -740,9 +740,12 @@ class AdminController extends Controller
             if ($forceRefresh) {
                 $result = $fetchData();
                 \App\Core\Cache::put($cacheKey, $result, 1);
-                $result['refreshed'] = true;
+                $result['source'] = 'fresh';
+                $result['last_updated'] = date('H:i:s');
             } else {
                 $result = \App\Core\Cache::remember($cacheKey, 1, $fetchData);
+                if (!isset($result['source'])) $result['source'] = 'cache';
+                if (!isset($result['last_updated'])) $result['last_updated'] = date('H:i:s');
             }
 
             // Real-time Online Stats (Always fetch, never cache)
